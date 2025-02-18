1a7b6b084e5039b355096962c99ca2043e5e4e04
nguyendinhutbiu <nguyendinhutbiu@gmail.com>
2025-02-18 23:21:16 +0700

    feat: cls hook

---
+++ src/middlewares/clsHooked.js
@@ -0,0 +1,135 @@
+const _ = require('lodash');
+const { createNamespace, getNamespace } = require('cls-hooked');
+const logger = require('../config/logger');
+const { language, countries, SESSION_NAME_SPACE } = require('../utils/constant');
+
+if (!getNamespace(SESSION_NAME_SPACE)) {
+  createNamespace(SESSION_NAME_SPACE);
+}
+const CLS_HOOK_KEYS = 'ALL_CLS_HOOK_KEY';
+
+const clsHooked = (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return;
+  }
+  let clsSession;
+  if (!getNamespace(SESSION_NAME_SPACE)) {
+    clsSession = createNamespace(SESSION_NAME_SPACE);
+  } else {
+    clsSession = getNamespace(SESSION_NAME_SPACE);
+  }
+  clsSession.run(() => {
+    clsSession.set('clientLanguage', _.get(req, 'headers.lang') || language.english);
+    clsSession.set('path', _.get(req, 'path') || '');
+    next();
+  });
+};
+
+const getSession = ({ key }) => {
+  try {
+    const clsSession = getNamespace(SESSION_NAME_SPACE);
+    if (clsSession) {
+      return clsSession.get(key);
+    }
+    return null;
+  } catch (err) {
+    const message = `error set session errStack = ${err.stack}. `;
+    logger.error(message);
+  }
+};
+
+const _putKeysForAllClsHook = (key) => {
+  try {
+    const clsSession = getNamespace(SESSION_NAME_SPACE);
+    let clsHookAllKeys = getSession({ key: CLS_HOOK_KEYS }) || [];
+    clsHookAllKeys = [...clsHookAllKeys, key];
+    clsHookAllKeys = _.uniq(clsHookAllKeys);
+    clsSession.set(CLS_HOOK_KEYS, clsHookAllKeys);
+    // eslint-disable-next-line
+  } catch (err) {}
+};
+
+const setSession = ({ key, value }) => {
+  try {
+    const clsSession = getNamespace(SESSION_NAME_SPACE);
+    if (clsSession) {
+      clsSession.set(key, value);
+    }
+    _putKeysForAllClsHook(key);
+    return true;
+  } catch (err) {
+    const message = `error set session errStack = ${err.stack}.`;
+    logger.error(message);
+    return false;
+  }
+};
+
+const setEmployeePermissions = (permissions) => {
+  setSession({ key: 'permissions', value: permissions });
+};
+
+const getEmployeePermissions = () => {
+  return getSession({ key: 'permissions' }) || [];
+};
+
+const setRestaurantToSession = (restaurantJson) => {
+  return setSession({ key: 'restaurant', value: restaurantJson });
+};
+
+const getRestaurantFromSession = () => {
+  return getSession({ key: 'restaurant' });
+};
+
+const getRestaurantTimeZone = () => {
+  const restaurant = getRestaurantFromSession();
+  const utcOffSet = _.get(restaurant, 'utcOffset') || 7;
+  if (utcOffSet === 9) {
+    return 'Asia/Tokyo';
+  }
+
+  if (utcOffSet === 7) {
+    return 'Asia/Ho_Chi_Minh';
+  }
+  if (_.get(restaurant, 'timezone')) {
+    return restaurant.timezone;
+  }
+  return 'Asia/Ho_Chi_Minh';
+};
+
+const getRestaurantCountry = () => {
+  const restaurant = getRestaurantFromSession();
+  return _.get(restaurant, 'country') || countries.VietNam.name;
+};
+
+const getRestaurantCurrency = () => {
+  const restaurant = getRestaurantFromSession();
+  return _.get(restaurant, 'currency') || countries.VietNam.currency;
+};
+
+const getRestaurantLang = () => {
+  const restaurant = getRestaurantFromSession();
+  const country = _.get(restaurant, 'country');
+  if (country === countries.VietNam.name) {
+    return 'vi';
+  }
+  return 'en';
+};
+
+const getClientLanguageWithHook = () => {
+  return getSession({ key: 'clientLanguage' }) || getRestaurantLang();
+};
+
+module.exports = {
+  clsHooked,
+  setSession,
+  getSession,
+  setRestaurantToSession,
+  getRestaurantFromSession,
+  getRestaurantCurrency,
+  getRestaurantTimeZone,
+  getRestaurantCountry,
+  getRestaurantLang,
+  setEmployeePermissions,
+  getEmployeePermissions,
+  getClientLanguageWithHook,
+};
---
+++ src/utils/constant.js
@@ -0,0 +1,19 @@
+const SESSION_NAME_SPACE = 'userSession';
+
+const language = {
+  vietnamese: 'vi',
+  english: 'en',
+};
+
+const countries = {
+  VietNam: {
+    name: 'Việt Nam',
+    currency: 'VND',
+  },
+};
+
+module.exports = {
+  SESSION_NAME_SPACE,
+  language,
+  countries,
+};

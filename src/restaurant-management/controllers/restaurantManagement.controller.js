91284777fc792f7ad112f1ccf0f3c593b43ec96b
kipfuho <ctcakip@gmail.com>
2025-02-28 16:09:03 +0700

    feat: add controller + route restaurant management

--- src/restaurant-management/controllers/restaurantManagement.controller.js
+++ src/restaurant-management/controllers/restaurantManagement.controller.js
@@ -28,9 +28,154 @@ const deleteRestaurant = catchAsync(async (req, res) => {
   res.status(httpStatus.OK).send({ message: 'Xoá thành công' });
 });
 
+const getTable = catchAsync(async (req, res) => {
+  const tableId = _.get(req, 'params.tableId');
+  const table = await restaurantManagementService.getTable(tableId);
+  res.status(httpStatus.OK).send({ table });
+});
+
+const createTable = catchAsync(async (req, res) => {
+  const createBody = req.body;
+  const table = await restaurantManagementService.createTable(createBody);
+  res.status(httpStatus.CREATED).send({ table });
+});
+
+const updateTable = catchAsync(async (req, res) => {
+  const tableId = _.get(req, 'params.tableId');
+  const updateBody = req.body;
+  await restaurantManagementService.updateTable(tableId, updateBody);
+  res.status(httpStatus.OK).send({ message: 'Cập nhật thành công' });
+});
+
+const deleteTable = catchAsync(async (req, res) => {
+  const tableId = _.get(req, 'params.tableId');
+  await restaurantManagementService.deleteTable(tableId);
+  res.status(httpStatus.OK).send({ message: 'Xoá thành công' });
+});
+
+const getTablePosition = catchAsync(async (req, res) => {
+  const tablePositionId = _.get(req, 'params.tablePositionId');
+  const tablePosition = await restaurantManagementService.getTablePosition(tablePositionId);
+  res.status(httpStatus.OK).send({ tablePosition });
+});
+
+const createTablePosition = catchAsync(async (req, res) => {
+  const createBody = req.body;
+  const tablePosition = await restaurantManagementService.createTablePosition(createBody);
+  res.status(httpStatus.CREATED).send({ tablePosition });
+});
+
+const updateTablePosition = catchAsync(async (req, res) => {
+  const tablePositionId = _.get(req, 'params.tablePositionId');
+  const updateBody = req.body;
+  await restaurantManagementService.updateTablePosition(tablePositionId, updateBody);
+  res.status(httpStatus.OK).send({ message: 'Cập nhật thành công' });
+});
+
+const deleteTablePosition = catchAsync(async (req, res) => {
+  const tablePositionId = _.get(req, 'params.tablePositionId');
+  await restaurantManagementService.deleteTablePosition(tablePositionId);
+  res.status(httpStatus.OK).send({ message: 'Xoá thành công' });
+});
+
+const getEmployee = catchAsync(async (req, res) => {
+  const employeeId = _.get(req, 'params.employeeId');
+  const employee = await restaurantManagementService.getEmployee(employeeId);
+  res.status(httpStatus.OK).send({ employee });
+});
+
+const createEmployee = catchAsync(async (req, res) => {
+  const createBody = req.body;
+  const employee = await restaurantManagementService.createEmployee(createBody);
+  res.status(httpStatus.CREATED).send({ employee });
+});
+
+const updateEmployee = catchAsync(async (req, res) => {
+  const employeeId = _.get(req, 'params.employeeId');
+  const updateBody = req.body;
+  await restaurantManagementService.updateEmployee(employeeId, updateBody);
+  res.status(httpStatus.OK).send({ message: 'Cập nhật thành công' });
+});
+
+const deleteEmployee = catchAsync(async (req, res) => {
+  const employeeId = _.get(req, 'params.employeeId');
+  await restaurantManagementService.deleteEmployee(employeeId);
+  res.status(httpStatus.OK).send({ message: 'Xoá thành công' });
+});
+
+const getEmployeePosition = catchAsync(async (req, res) => {
+  const employeePositionId = _.get(req, 'params.employeePositionId');
+  const employeePosition = await restaurantManagementService.getEmployeePosition(employeePositionId);
+  res.status(httpStatus.OK).send({ employeePosition });
+});
+
+const createEmployeePosition = catchAsync(async (req, res) => {
+  const createBody = req.body;
+  const employeePosition = await restaurantManagementService.createEmployeePosition(createBody);
+  res.status(httpStatus.CREATED).send({ employeePosition });
+});
+
+const updateEmployeePosition = catchAsync(async (req, res) => {
+  const employeePositionId = _.get(req, 'params.employeePositionId');
+  const updateBody = req.body;
+  await restaurantManagementService.updateEmployeePosition(employeePositionId, updateBody);
+  res.status(httpStatus.OK).send({ message: 'Cập nhật thành công' });
+});
+
+const deleteEmployeePosition = catchAsync(async (req, res) => {
+  const employeePositionId = _.get(req, 'params.employeePositionId');
+  await restaurantManagementService.deleteEmployeePosition(employeePositionId);
+  res.status(httpStatus.OK).send({ message: 'Xoá thành công' });
+});
+
+const getDepartment = catchAsync(async (req, res) => {
+  const departmentId = _.get(req, 'params.departmentId');
+  const department = await restaurantManagementService.getDepartment(departmentId);
+  res.status(httpStatus.OK).send({ department });
+});
+
+const createDepartment = catchAsync(async (req, res) => {
+  const createBody = req.body;
+  const department = await restaurantManagementService.createDepartment(createBody);
+  res.status(httpStatus.CREATED).send({ department });
+});
+
+const updateDepartment = catchAsync(async (req, res) => {
+  const departmentId = _.get(req, 'params.departmentId');
+  const updateBody = req.body;
+  await restaurantManagementService.updateDepartment(departmentId, updateBody);
+  res.status(httpStatus.OK).send({ message: 'Cập nhật thành công' });
+});
+
+const deleteDepartment = catchAsync(async (req, res) => {
+  const departmentId = _.get(req, 'params.departmentId');
+  await restaurantManagementService.deleteDepartment(departmentId);
+  res.status(httpStatus.OK).send({ message: 'Xoá thành công' });
+});
+
 module.exports = {
   getRestaurant,
   createRestaurant,
   updateRestaurant,
   deleteRestaurant,
+  getTable,
+  createTable,
+  updateTable,
+  deleteTable,
+  getTablePosition,
+  createTablePosition,
+  updateTablePosition,
+  deleteTablePosition,
+  getEmployee,
+  createEmployee,
+  updateEmployee,
+  deleteEmployee,
+  getEmployeePosition,
+  createEmployeePosition,
+  updateEmployeePosition,
+  deleteEmployeePosition,
+  getDepartment,
+  createDepartment,
+  updateDepartment,
+  deleteDepartment,
 };
--- src/restaurant-management/routes/restaurantManagement.route.js
+++ src/restaurant-management/routes/restaurantManagement.route.js
@@ -9,4 +9,29 @@ router.post('/create', auth(), restaurantManagementController.createRestaurant);
 router.patch('/:restaurantId', auth(), restaurantManagementController.updateRestaurant);
 router.delete('/:restaurantId', auth(), restaurantManagementController.deleteRestaurant);
 
+router.get('/:tableId', auth(), restaurantManagementController.getTable);
+router.post('/create', auth(), restaurantManagementController.createTable);
+router.patch('/:tableId', auth(), restaurantManagementController.updateTable);
+router.delete('/:tableId', auth(), restaurantManagementController.deleteTable);
+
+router.get('/:tablePositionId', auth(), restaurantManagementController.getTablePosition);
+router.post('/create', auth(), restaurantManagementController.createTablePosition);
+router.patch('/:tablePositionId', auth(), restaurantManagementController.updateTablePosition);
+router.delete('/:tablePositionId', auth(), restaurantManagementController.deleteTablePosition);
+
+router.get('/:employeeId', auth(), restaurantManagementController.getEmployee);
+router.post('/create', auth(), restaurantManagementController.createEmployee);
+router.patch('/:employeeId', auth(), restaurantManagementController.updateEmployee);
+router.delete('/:employeeId', auth(), restaurantManagementController.deleteEmployee);
+
+router.get('/:employeePositionId', auth(), restaurantManagementController.getEmployeePosition);
+router.post('/create', auth(), restaurantManagementController.createEmployeePosition);
+router.patch('/:employeePositionId', auth(), restaurantManagementController.updateEmployeePosition);
+router.delete('/:employeePositionId', auth(), restaurantManagementController.deleteEmployeePosition);
+
+router.get('/:departmentId', auth(), restaurantManagementController.getDepartment);
+router.post('/create', auth(), restaurantManagementController.createDepartment);
+router.patch('/:departmentId', auth(), restaurantManagementController.updateDepartment);
+router.delete('/:departmentId', auth(), restaurantManagementController.deleteDepartment);
+
 module.exports = router;

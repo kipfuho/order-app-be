a6dbb17678dba6c34e19aecce2cd162a10bb554c
kipfuho <ctcakip@gmail.com>
2025-03-09 00:20:13 +0700

    feat: auth

--- src/config/config.js
+++ src/config/config.js
@@ -26,6 +26,8 @@ const envVarsSchema = Joi.object()
     SMTP_USERNAME: Joi.string().description('username for email server'),
     SMTP_PASSWORD: Joi.string().description('password for email server'),
     EMAIL_FROM: Joi.string().description('the from field in the emails sent by the app'),
+    CUSTOMER_APPID: Joi.string().description('appid for customer request'),
+    RESTAURANT_APPID: Joi.string().description('appid for restaurant request'),
   })
   .unknown();
 
@@ -66,5 +68,9 @@ module.exports = {
       },
     },
     from: envVars.EMAIL_FROM,
+  },
+  appid: {
+    customer: envVars.CUSTOMER_APPID,
+    restaurant: envVars.RESTAURANT_APPID,
   },
 };
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,7 +1,33 @@
+const _ = require('lodash');
 const passport = require('passport');
 const httpStatus = require('http-status');
 const ApiError = require('../utils/ApiError');
-const { roleRights } = require('../config/roles');
+const { roleRights, roles } = require('../config/roles');
+const { appid } = require('../config/config');
+const { getRestaurantFromCache, getEmployeeFromCache } = require('../metadata/restaraurantMetadata.service');
+
+const getAppId = (req) => _.get(req, 'headers.appid') || '';
+const isCustomerRequest = (req) => {
+  const appId = getAppId(req);
+  return appId.includes(appid.customer);
+};
+
+const isRestaurantRequest = (req) => {
+  const appId = getAppId(req);
+  return appId.includes(appid.restaurant);
+};
+
+const _verifyAdmin = (req, requiredRights) => {
+  const { user } = req;
+  if (user.role === roles.admin && requiredRights.length) {
+    const userRights = roleRights.get(user.role);
+    const hasRequiredRights = requiredRights.every((requiredRight) => userRights.includes(requiredRight));
+    if (!hasRequiredRights && req.params.userId !== user.id) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const verifyCallback = (req, resolve, reject, requiredRights) => async (err, user, info) => {
   if (err || info || !user) {
@@ -9,11 +35,34 @@ const verifyCallback = (req, resolve, reject, requiredRights) => async (err, use
   }
   req.user = user;
 
-  if (requiredRights.length) {
-    const userRights = roleRights.get(user.role);
-    const hasRequiredRights = requiredRights.every((requiredRight) => userRights.includes(requiredRight));
-    if (!hasRequiredRights && req.params.userId !== user.id) {
-      return reject(new ApiError(httpStatus.FORBIDDEN, 'Forbidden'));
+  if (!_verifyAdmin(req, requiredRights)) {
+    return reject(new ApiError(httpStatus.FORBIDDEN, 'Forbidden'));
+  }
+
+  req.isCustomerRequest = isCustomerRequest(req);
+  req.isRestaurantRequest = isRestaurantRequest(req);
+
+  let { restaurantId } = req;
+  if (!restaurantId) {
+    restaurantId = req.params.restaurantId || _.get(req, 'body.restaurantId');
+  }
+  if (!restaurantId) {
+    resolve();
+    return;
+  }
+
+  const restaurant = await getRestaurantFromCache({ restaurantId });
+
+  if (restaurant.userId.toString() !== user.id) {
+    const employee = await getEmployeeFromCache({ userId: user.id, restaurantId });
+    if (!employee) {
+      return reject(new ApiError(httpStatus.NOT_FOUND, 'Không tìm thấy nhân viên'));
+    }
+    req.employee = employee;
+    const { permissions } = employee;
+    const hasPermissions = requiredRights.every((right) => permissions.includes(right));
+    if (!hasPermissions) {
+      return reject(new ApiError(httpStatus.FORBIDDEN, 'Không có đủ quyền'));
     }
   }
 
